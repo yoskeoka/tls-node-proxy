292ed166094d273feefd21c4f4f4f3f83799d408
fuyu <yoske95@gmail.com>
2016-04-21 23:06:28 +0900

    test ssl

--- proxy.js
+++ proxy.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var http2 = require('http2');
 var httpProxy = require('http-proxy');
 
 var sslkeyPath = '/etc/letsencrypt/live/goita.beta.or.jp/'
 var options = {
     key: fs.readFileSync(sslkeyPath + 'privkey.pem', 'utf8'),
     cert: fs.readFileSync(sslkeyPath + 'cert.pem', 'utf8')
-  } 
+}
+  
 var proxy = httpProxy.createProxyServer({
     target: "http://goita.beta.or.jp:8880",
     ws: true      
@@ -25,10 +25,12 @@ var sslproxy = httpProxy.createServer({
     target: "http://goita.beta.or.jp:8880",
     ws: true,
     secure: true,
-    ssl:{
-        key: fs.readFileSync(sslkeyPath + 'privkey.pem', 'utf8'),
-        cert: fs.readFileSync(sslkeyPath + 'cert.pem', 'utf8')
-    }   
+    ssl: options  
+});
+
+sslproxy.on('upgrade', function (req, socket, head) {
+    console.log("ssl websocket!");
+    proxy.ws(req, socket, head);
 });
 
 server.listen(80); //root is required to run service on port 80.
