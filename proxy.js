9bd7a6d4a6e789d3eac2766d3b2fef12e2d920ae
fuyu <yoske95@gmail.com>
2016-04-21 22:37:38 +0900

    test commit

--- proxy.js
+++ proxy.js
@@ -1,27 +1,33 @@
 var fs = require('fs');
 var http = require('http');
+var http2 = require('http2');
 var httpProxy = require('http-proxy');
 
 var sslkeyPath = '/etc/letsencrypt/live/goita.beta.or.jp/'
-
-var proxy = httpProxy.createProxyServer({
-    target: "http://goita.beta.or.jp:8880",
-    ws: true,
-    secure: true,
-    ssl: {
+var options = {
     key: fs.readFileSync(sslkeyPath + 'privkey.pem', 'utf8'),
     cert: fs.readFileSync(sslkeyPath + 'cert.pem', 'utf8')
-  }   
+  } 
+var proxy = httpProxy.createProxyServer({
+    target: "http://goita.beta.or.jp:8880",
+    ws: true      
 });
 
 var server = http.createServer(function(req, res){
     proxy.web(req, res);
 });
-
 server.on('upgrade', function (req, socket, head) {
     proxy.ws(req, socket, head);
 });
 
-server.listen(8080); //root is required to run service on port 80.
+var server2 = http2.createServer(options,function(req, res){
+    proxy.web(req, res);
+});
 
+server2.on('upgrade', function (req, socket, head) {
+    proxy.ws(req, socket, head);
+});
+
+server.listen(80); //root is required to run service on port 80.
+server2.listen(443); //root is required to run service on port 443.
 console.log("proxy server started");
